62f24fd03846fc1f9ab71f652265e8aac9f27005
elbeg-erdene <85168532+elbeg-erdene@users.noreply.github.com>
2026-07-20 12:21:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ window.open(
 paidAddressBtn.onclick = function(){
 
 window.open(
-"https://sites.google.com/view/kimigo-nalaikh/mn-%D1%82%D1%8D%D0%BC%D0%B4%D1%8D%D0%B3%D0%B3%D2%AF%D0%B9-%D0%B0%D1%87%D0%B0%D0%B0-%D1%85%D2%AF%D0%BB%D1%8D%D1%8D%D0%B6-%D0%B0%D0%B2%D0%B0%D1%85-%D1%85%D0%B0%D1%8F%D0%B3-%D1%85%D0%BE%D0%BB%D0%B1%D0%BE%D1%85?authuser=0",
+"https://www.elbegcargo.com/",
 "_blank");
 
 };
